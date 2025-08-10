148e1e62f44b43c53a51f1a61a8d9b3cdfa8ad18
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-10 07:44:53 +0600

    even average

--- odd_even_average.js
+++ odd_even_average.js
@@ -1,16 +1,16 @@
 // odd avg--------
-function oddNum(numbers){
+function oddNum(numbers) {
     let odd = [];
     for (const number of numbers) {
-        if(number % 2 === 1){
-           odd.push(number);
-            
-        }        
-    }  
+        if (number % 2 === 1) {
+            odd.push(number);
+
+        }
+    }
     let sum = 0;
     for (const oddNumber of odd) {
-        sum = sum + oddNumber;      
-        
+        sum = sum + oddNumber;
+
     }
 
     const length = odd.length
@@ -21,4 +21,34 @@ function oddNum(numbers){
 const array = [12, 15, 19, 14, 17, 16, 91];
 const oddAvgResult = oddNum(array);
 console.log(oddAvgResult);
+
+
+// even avg----------
+function evenNum(num) {
+    let evenArray = []
+    for (const element of num) {
+        // console.log(element);
+        if (element % 2 === 0) {
+            evenArray.push(element)
+        }
+
+    }
+    // console.log(evenArray);
+
+    let evenArraySum = 0;
+    for (const element of evenArray) {
+        // console.log(element);
+
+        evenArraySum += element
+        // console.log(evenArraySum);
+        
+    }
+    const arrayLength = evenArray.length;
+    const avg = evenArraySum / arrayLength;
+    return avg;
+
+}
+
+const evenAvgResult = evenNum(array);
+console.log(evenAvgResult);
 
