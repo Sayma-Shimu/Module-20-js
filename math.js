fb66640704671556dff7d1c451401663fcfb7090
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-10 11:34:17 +0600

    math

---
+++ math.js
@@ -0,0 +1,42 @@
+const min = Math.min(45, 15, 25, 21, 31);
+console.log(min);
+
+const max = Math.max(45, 15, 25, 21, 31);
+console.log(max);
+
+// pai------
+console.log(Math.PI);
+
+// Absolute--> positive number dibe--------
+console.log(Math.abs(-88));
+console.log(Math.abs(88));
+console.log(Math.abs(10-5));
+console.log(Math.abs(5-10));
+
+// Round--> nikotborti purono shongkay rupannator hobe--------
+console.log(Math.round(4.25));
+console.log(Math.round(5.85));
+console.log(Math.round(4.44));
+
+
+// floor--> vognagsho jotoi hok eta always purnoshongka dibe-----
+console.log(Math.floor(5.99));
+console.log(Math.floor(2.22));
+
+
+// ceil--> vognagsho jotoi hok eta tene 1 bariye felbe-------
+console.log(Math.ceil(3.09));
+console.log(Math.ceil(2.000002));
+
+
+// random--> 0 theke 1 er moddhe doshomikwala numbr dibe randomly-------
+console.log(Math.random());
+console.log(Math.random());
+
+// 10 diye gun korle 1-10er modde doshomikwala numbr dibe randomly-------
+console.log(Math.random()*10);
+
+// 1-10er modde purno shongka korte chaile-------
+const rand = Math.round(Math.random()*10)
+console.log(rand);
+
